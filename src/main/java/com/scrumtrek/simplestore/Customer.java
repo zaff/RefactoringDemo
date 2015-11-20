d5033ca605ed41cd0076dbce1787b26f3ca9f2a3
zaff <mr.zaff@gmail.com>
2015-11-20 12:12:13 +0300

    string.format

--- src/main/java/com/scrumtrek/simplestore/Customer.java
+++ src/main/java/com/scrumtrek/simplestore/Customer.java
@@ -1,7 +1,10 @@
 package com.scrumtrek.simplestore;
 
+import org.omg.CORBA.Environment;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Customer {
 	private final transient String m_Name;
@@ -24,7 +27,7 @@ public class Customer {
 		double totalAmount = 0;
 		int totalPoints = 0;
 
-		String result = "Rental record for " + m_Name + "\n";
+		StringBuilder result = new StringBuilder("Rental record for ").append(m_Name).append("\n");
 
 		for (Rental each : m_Rentals) {
 
@@ -34,15 +37,15 @@ public class Customer {
 			int days = each.getDaysRented();
 
 			double amount = category.CalculateAmount(days);
-			result += "\t" + movie.getTitle() + "\t" + amount + "\n";
+			result.append(String.format(Locale.US, "\t%s\t%.1f\n", movie.getTitle(), amount));
 			totalAmount += amount;
 			totalPoints += category.CalculatePoints(days);
 		}
 
 		// Add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + totalPoints + " frequent renter points.";
-		return result;
+        result.append(String.format(Locale.US, "Amount owed is %.1f\n", totalAmount));
+        result.append(String.format("You earned %d frequent renter points.", totalPoints));
+		return result.toString();
 	}
 }
 
