15840edde6219e039ab95a193a5f73d779c8c2ef
zaff <mr.zaff@gmail.com>
2015-11-20 15:17:04 +0300

    no more tabs

--- src/main/java/com/scrumtrek/simplestore/Rental.java
+++ src/main/java/com/scrumtrek/simplestore/Rental.java
@@ -1,20 +1,20 @@
 package com.scrumtrek.simplestore;
 
 public class Rental {
-	private final Movie movie;
-	private final int daysRented;
+    private final Movie movie;
+    private final int daysRented;
 
-	public Rental(Movie movie, int daysRented) {
-		this.movie = movie;
-		this.daysRented = daysRented;
-	}
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
 
-	public int getDaysRented() {
-		return daysRented;
-	}
+    public int getDaysRented() {
+        return daysRented;
+    }
 
-	public Movie getMovie() {
-		return movie;
-	}
+    public Movie getMovie() {
+        return movie;
+    }
 }
 
