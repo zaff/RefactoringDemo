aad92476048b2bc92d87b9a3897bad3f9a85e83f
zaff <mr.zaff@gmail.com>
2015-11-20 13:21:21 +0300

    fix

--- src/main/java/com/scrumtrek/simplestore/Customer.java
+++ src/main/java/com/scrumtrek/simplestore/Customer.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 
 public class Customer {
     private final transient String m_Name;
-    private final transient List<Rental> m_Rentals = new ArrayList<>();
+    private final List<Rental> m_Rentals = new ArrayList<>();
 
     public Customer(String name) {
         m_Name = name;
@@ -26,14 +26,12 @@ public class Customer {
 
         StringBuilder result = new StringBuilder("Rental record for ").append(m_Name).append(System.lineSeparator());
 
-        for (Rental each : m_Rentals) {
+        for (final Rental each : m_Rentals) {
+            final Movie movie = each.getMovie();
+            final IRentalCategory category = movie.getRentalCategory();
+            final int days = each.getDaysRented();
+            final double amount = category.CalculateAmount(days);
 
-            Movie movie = each.getMovie();
-            IRentalCategory category = movie.getRentalCategory();
-
-            int days = each.getDaysRented();
-
-            double amount = category.CalculateAmount(days);
             result.append(String.format(Locale.US, "\t%s\t%.1f%s", movie.getTitle(), amount, System.lineSeparator()));
             totalAmount += amount;
             totalPoints += category.CalculatePoints(days);
