38202bb06c279dc27969a781e6e5cb2b0e6a8770
zaff <mr.zaff@gmail.com>
2015-11-20 15:15:04 +0300

    tabs

--- src/main/java/com/scrumtrek/simplestore/Movie.java
+++ src/main/java/com/scrumtrek/simplestore/Movie.java
@@ -1,20 +1,20 @@
 package com.scrumtrek.simplestore;
 
 public class Movie {
-	private final String title;
-	private final IRentalCategory rentalCategory;
+    private final String title;
+    private final IRentalCategory rentalCategory;
 
-	public Movie(String title, IRentalCategory rentalCategory) {
-		this.title = title;
-		this.rentalCategory = rentalCategory;
-	}
+    public Movie(String title, IRentalCategory rentalCategory) {
+        this.title = title;
+        this.rentalCategory = rentalCategory;
+    }
 
-	public IRentalCategory getRentalCategory() {
-		return rentalCategory;
-	}
+    public IRentalCategory getRentalCategory() {
+        return rentalCategory;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 }
 
