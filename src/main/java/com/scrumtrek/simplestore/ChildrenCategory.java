85887b63df4acd2d57528e1229ce03851e47a5bc
zaff <mr.zaff@gmail.com>
2015-11-20 12:43:39 +0300

    cs

--- src/main/java/com/scrumtrek/simplestore/ChildrenCategory.java
+++ src/main/java/com/scrumtrek/simplestore/ChildrenCategory.java
@@ -4,11 +4,10 @@ package com.scrumtrek.simplestore;
  * Created by user on 19.11.2015.
  */
 public class ChildrenCategory extends  OnePointCategoryBase {
-    final static double amountPerDay = 1.5;
-    final static int actionDayCount = 3;
-
     @Override
     public double CalculateAmount(int days) {
+        final double amountPerDay = 1.5;
+        final int actionDayCount = 3;
         if (days > actionDayCount) {
             return (days - actionDayCount) * amountPerDay;
         }
